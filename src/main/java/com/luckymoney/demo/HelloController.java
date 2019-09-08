007cb2953c4ffab4ef6cdddfa996af8b1b5acfc4
187092208767 <1214023762@qq.com>
2019-09-09 02:21:46 +0800

    程旭第二次idea和git版本控制器练习

--- src/main/java/com/luckymoney/demo/HelloController.java
+++ src/main/java/com/luckymoney/demo/HelloController.java
@@ -3,11 +3,16 @@ package com.luckymoney.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-@RestController
+
+/**
+ * @Controller+@ResponseBody==@RestController
+ */
+@Controller
+@ResponseBody
 public class HelloController {
     @Autowired
     private LimitConfig limitConfig;
